7dd28a21de43c8e57e20a6af8b2ade5c49503d72
Andrew Block <blockan@mnstate.edu>
2016-10-30 16:23:02 -0500

    Add Eli things

--- about_me.py
+++ about_me.py
@@ -8,6 +8,6 @@ print("My name is Laura and I   ")
 
 print("My name is Erin and I    ")
 
-print("My name is Elias and I   ")
+print("My name is Elias and I am really cool  ")
 
 print("My name is Stefan and I don't matter")
